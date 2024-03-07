19ae74f9b1b7e1dc0d5995b537929cfd48751f0a
乘风 <jian.shu@hipac.cn>
2024-03-07 17:12:14 +0800

    msg server

--- run.py
+++ run.py
@@ -1,28 +1,25 @@
 # 创建应用实例
-import json
+from gevent import monkey
+monkey.patch_all()
 import sys
-
+import json
 import gevent
 import requests
-from gevent import monkey
 import threading
-
 from gevent.queue import Empty
-
 from wxcloudrun.WebsocketClient import WebsocketCWrap
-# from wxcloudrun.views import background_task
-
-monkey.patch_all()
-
 from gevent.pywsgi import WSGIServer
 from wxcloudrun import app, serverMsgQueue
 
+wsc = WebsocketCWrap()
+
+def runWebsocket():
+    wsc.run()
 
 def background_task():
-    wsc = WebsocketCWrap()
-    wsc.run()
     while True:
         try:
+            app.logger.warn("运行中")
             item = serverMsgQueue.get(timeout=1)
             if item is not None:
                 app.logger.warn(f'开始发送消息:{item}')
@@ -38,6 +35,10 @@ if __name__ == '__main__':
     # app.run()
     # socketio.run(app, host=sys.argv[1], port=sys.argv[2])
     # app.run(host=sys.argv[1], port=sys.argv[2])
+
+    runConnThread = threading.Thread(target=runWebsocket)
+    runConnThread.daemon = True  # 设置为守护线
+    runConnThread.start()
 
     thread = threading.Thread(target=background_task)
     thread.daemon = True  # 设置为守护线程，以便 Flask 应用退出时自动结束
