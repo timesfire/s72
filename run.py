7fbf314b0d18d2d42337f7c69fd54fa025f81f79
乘风 <jian.shu@hipac.cn>
2024-03-07 16:31:11 +0800

    msg server

--- run.py
+++ run.py
@@ -25,7 +25,9 @@ def background_task():
         try:
             item = serverMsgQueue.get(timeout=1)
             if item is not None:
+                app.logger.warn(f'开始发送消息:{item}')
                 if not wsc.sendMsg(json.dumps(item)): # socket 发送失败，用http
+                    app.logger.warn("发送失败")
                     requests.post(url="http://msg-notify-88593-7-1323709807.sh.run.tcloudbase.com/notifyWs",json=item)
         except Empty:
             pass
--- wxcloudrun/WebsocketClient.py
+++ wxcloudrun/WebsocketClient.py
@@ -1,9 +1,11 @@
 import websocket
 
+from wxcloudrun import app
+
 
 class WebsocketCWrap:
     def __init__(self):
-        websocket.enableTrace(True)
+        websocket.enableTrace(False)
         self.ws = websocket.WebSocketApp("ws://msg-notify-88593-7-1323709807.sh.run.tcloudbase.com/notifyWsx",
                                          on_open=self.on_open,
                                          on_message=self.on_message,
@@ -11,17 +13,18 @@ class WebsocketCWrap:
                                          on_close=self.on_close)
         self.isOpen = False
     def on_message(self, ws, message):
-        print(message)
+        app.logger.warn(message)
 
     def on_error(self, ws, error):
-        print(error)
+        app.logger.warn(error)
         self.isOpen = False
 
     def on_close(self, ws, close_status_code, close_msg):
-        print("### closed ###")
+        app.logger.warn("### closed ###")
         self.isOpen = False
 
     def on_open(self, ws):
+        app.logger.warn("Opened connection")
         print("Opened connection")
         self.isOpen = True
 
